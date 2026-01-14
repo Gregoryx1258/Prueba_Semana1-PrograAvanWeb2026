a9f10da39dfc19b04056c88e366709c8e0cdd947
Gregoryx1258 <102883971+Gregoryx1258@users.noreply.github.com>
2026-01-13 20:28:55 -0600

    Proyecto

---
+++ Prueba 1/Pages/AcercaDe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Prueba_1.Pages
+{
+    public class AcercaDeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Prueba 1/Pages/Shared/AcercaDe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Prueba_1.Pages.Shared
+{
+    public class AcercaDeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
